696d5e5b7e7b99ee3c7eff75f5b61c6940cbd243
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-11-02 23:06:20 -0800

    Created a new Project

---
+++ WebBowserDragAndDrop/src/webbowserdraganddrop/WebBowserDragAndDrop.java
@@ -0,0 +1,17 @@
+
+package webbowserdraganddrop;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class WebBowserDragAndDrop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
